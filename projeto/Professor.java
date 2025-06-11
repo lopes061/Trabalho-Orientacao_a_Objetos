1cbc5ea9930420676d913fa0bd512e035209d864
Enzo Lopes <72364634+lopes061@users.noreply.github.com>
2025-06-11 18:55:00 -0300

    Update Professor.java

--- projeto/Professor.java
+++ projeto/Professor.java
@@ -5,11 +5,12 @@ public class Professor extends Servidores {
     private String cargoAcademico;
 
     public Professor(String nome, String email_institucional, String telefone, String senha,
-                     String matricula_institucional, String curso, String cargoAcademico) {
-        super(nome, email_institucional, telefone, senha, matricula_institucional);
-        this.curso = curso;
-        this.cargoAcademico = cargoAcademico;
-    }
+                 String matricula_institucional, String curso, String cargoAcademico) {
+    super(matricula_institucional, nome, email_institucional, telefone, senha);
+    this.curso = curso;
+    this.cargoAcademico = cargoAcademico;
+}
+
 
 
     public String imprimir() {
@@ -19,5 +20,20 @@ public class Professor extends Servidores {
                "Matrícula Institucional: " + matricula_institucional + "\n" +
                "Curso Ministrado: " + curso + "\n" +
                "Cargo Acadêmico: " + cargoAcademico+ "\n";
+    }
+     public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getCargoAcademico() {
+        return cargoAcademico;
+    }
+
+    public void setCargoAcademico(String cargoAcademico) {
+        this.cargoAcademico = cargoAcademico;
     }
 }
