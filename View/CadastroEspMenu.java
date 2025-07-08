8a9ef7e65313aeb6c15f5a26bf09c863aed139a9
EdHCG <ehcgurjao@gmail.com>
2025-07-08 18:06:59 -0300

    Update de cadastro de espaços

--- View/CadastroEspMenu.java
+++ View/CadastroEspMenu.java
@@ -3,16 +3,35 @@ package View;
 import javax.swing.JOptionPane;
 
 public class CadastroEspMenu {
-    public static int EspOpcoes() {
-		String opcoes = "Informe a opção desejada \n"
+    public static String lerTipoSala() {
+		int i;
+		do{
+		String opcoes = "Informe o tipo de sala desejada \n"
 				      + "1 - Cadastro de uma sala de aula \n"
                       + "2 - Cadastro de um laboratorio \n"
-                      + "3 - Cadastro de uma sala de estudos \n"
-				      + "0 - Sair";
+                      + "3 - Cadastro de uma sala de estudos \n";
 	
 		String strOpcao = JOptionPane.showInputDialog(opcoes);
-		int opcao = Integer.parseInt(strOpcao);
-	
-		return opcao;
+		i = Integer.parseInt(strOpcao);
+		}while (i != 1 || i != 2 || i != 3); 
+		String resposta = "";
+		if(i == 1) {
+			resposta = "Sala de Aula";
+		}else if(i == 2) {
+			resposta = "Laboratorio";
+		}else if(i == 3){
+			resposta = "Sala de estudos";
+		}
+		return resposta;
 	}
+	
+	public static String lerNomeSala(){
+		return JOptionPane.showInputDialog("Digite o nome da sala: ");
+	}
+
+	public static int lerCapacidadeSala(){
+		int resp = Integer.parseInt(JOptionPane.showInputDialog("Digite o nome da sala: "));
+		return resp;
+	}
+
 }
